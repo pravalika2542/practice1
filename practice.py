4adcad99715db747401f7a7c568ebd7f4c0f56c2
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-05 15:40:01 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("Image Similarity Retrieval")
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Choose an image...", type=["png"])
