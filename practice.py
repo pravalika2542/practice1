b54bc652824124f06bb912a95c0dbc3f09064e7f
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-05 15:59:27 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,14 @@
 import streamlit as st
+from keras.applications import vgg16
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import Model
+from keras.applications.imagenet_utils import preprocess_input
+from PIL import Image
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import os
+
 st.set_page_config(page_title='Similarity')
 st.title("Image Similarity Retrieval")
 uploaded_file = st.file_uploader("Choose an image...", type=["png"])
